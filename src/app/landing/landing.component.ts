8bc4d4c531a11bee55e671eaa8b8dc7a6691371b
João Paulo Borges <joaopaulo.borges.gm@hotmail.com>
2020-04-13 02:26:40 -0300

    adicionando landing page

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+  lightBg = false;
+  pikachuHover = false;
+  sqrtlHover = false;
+  bulbaHover = false;
+  charHover = false;
+
+  whos = this.pikachuHover || this.sqrtlHover || this.bulbaHover;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(['(max-width: 650px)'])
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(event: HTMLElement): void {
+    console.log(event.innerText);
+  }
+}
---
+++ src/app/landing/landing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingComponent } from './landing.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [LandingComponent],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [LandingComponent]
+})
+export class LandingModule { }
