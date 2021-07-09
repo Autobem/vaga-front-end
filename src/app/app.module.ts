79288886ca762a2f490421c9d5774909d4c8a9cd
Sinval Borges <sinborneto@yahoo.com.br>
2021-07-08 23:14:24 -0300

    teste-pokemon

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { MaterialModule } from "./material.module";
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from "./shared/shared.module";
+import { PokemonService } from "./services/pokemon.service";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonListComponent,
+    PokemonDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    SharedModule
+  ],
+  providers: [PokemonService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
