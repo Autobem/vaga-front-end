b03530813d9359e58395bf7c6aacf03e541bf37c
Aldrey <aldreybp@gmail.com>
2021-11-24 00:11:45 -0300

    configurando classe de serviço para se conectar com a api

--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaginasModule } from './paginas/paginas.module';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { PaginasModule } from './paginas/paginas.module';
     BrowserModule,
     AppRoutingModule,
     PaginasModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
