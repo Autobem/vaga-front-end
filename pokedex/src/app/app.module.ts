ede3eb7a8fa391b63522b08ac478d9119bd5dd88
rennan-sbarros <rennan_sbarros@hotmail.com>
2021-12-07 23:36:38 -0300

    Pokemon list creation and search method

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -13,7 +14,8 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- pokedex/src/app/shared/list-pokemons/list-pokemons.component.ts
+++ pokedex/src/app/shared/list-pokemons/list-pokemons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'list-pokemons',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-pokemons.component.scss']
 })
 export class ListPokemonsComponent implements OnInit {
+  private setAllPokemons: any;
+  public getAllPokemons: any;
 
-  constructor() { }
+  constructor(private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+      }
+    );
+  }
+
+  public getSearch(value: string){
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
   }
 
 }
